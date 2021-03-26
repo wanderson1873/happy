524fc75ff62873ab1e4ad7f79452358f653f5e1b
Wanderson Oliveira <wanderson1873@live.com>
2021-03-26 14:22:45 +0000

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,31 +1,9 @@
-import React from 'react';
-
+import Route from './routes'
 import './styles/global.css'
-import './styles/pages/landing.css'
-
-import logoImage from './images/logo.svg'
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImage} alt="Happy"/>
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanato e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>Setúbal</strong>
-          <span>Portugal</span>
-        </div>
-
-        <a href="" className="enter-app">
-          »
-        </a>
-      </div>
-    </div>
+    <Route />
   );
 }
 
