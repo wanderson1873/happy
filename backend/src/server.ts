aa587a14455e86947c71ed8025b3e1f2b3a489a4
Wanderson Oliveira <wanderson1873@live.com>
2021-03-27 22:41:06 +0000

    confg bd orphanages

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,37 @@
 import express from 'express'
+import {getRepository} from 'typeorm'
+import Orphanage from './models/Orphanages'
+
 import './database/connection'
 
 const app = express()
 app.use(express.json())
 
-app.get('/user', (request, response) => {
-   return response.json({message: "Hello NLW"})
+app.post('/orphanages', async (request, response) => {
+   const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+   } = request.body
+
+   const orphanagesRepository = getRepository(Orphanage)
+   const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+   })
+
+   await orphanagesRepository.save(orphanage)
+
+   return response.status(201).json(orphanage)
 })
 
 app.listen(3333)
