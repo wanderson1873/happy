8abb41becb48c3d19e395a48cfd18a6bbb6904d5
Wanderson Oliveira <wanderson1873@live.com>
2021-03-28 20:19:46 +0100

    Tratando error

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express'
+import path from 'path'
+import cors from 'cors'
 import 'express-async-errors'
 import './database/connection'
 import routes from './routes'
-import path from 'path'
 import errorHandler from './errors/handle'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
