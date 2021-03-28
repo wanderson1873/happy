8094fc2e70132b50d7fa68ae246bbbd43c10053e
Wanderson Oliveira <wanderson1873@live.com>
2021-03-28 18:20:57 +0100

    tratando erros

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express'
+import 'express-async-errors'
 import './database/connection'
 import routes from './routes'
+import path from 'path'
+import errorHandler from './errors/handle'
 
 const app = express()
+
 app.use(express.json())
 app.use(routes)
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler)
 
 app.listen(3333)
