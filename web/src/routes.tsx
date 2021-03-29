4dfd9078d0cc5498bf9e4f4ce7a0e4491a3088a6
Wanderson Oliveira <wanderson1873@live.com>
2021-03-29 13:02:00 +0100

    Update routes.tsx
    add rotas (OrphanageCreate, Orphanage)

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,14 +1,19 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
 import Landing from './pages/Landing'
 import OrphanagesMap from './pages/OrphanagesMap'
+import CreateOrphanage from './pages/CreateOrphanage'
+import Orphanage from './pages/Orphanage'
 
 function Routes() {
    return (
       <BrowserRouter>
-         <switch>
+         <Switch>
             <Route path="/" exact component={Landing} />
-            <Route path="/app" exact component={OrphanagesMap} />
-         </switch>
+            <Route path="/app" component={OrphanagesMap} />
+            <Route path="orphanages/create" component={CreateOrphanage} />
+            <Route path="orphanages/:id" component={Orphanage} />
+         </Switch>
       </BrowserRouter>
    )
 }
